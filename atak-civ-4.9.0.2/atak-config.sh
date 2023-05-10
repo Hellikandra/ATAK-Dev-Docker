63667a087a96c9bdfe036d9516842dab2dbb51b9
Hellikandra <naevis.sharks@gmail.com>
2023-05-10 14:59:35 +0200

    corrections
    on 4.7.0.3 docker due to wrong name
    on 4.9.0.2 artifacts were still there.

--- atak-civ-4.9.0.2/atak-config.sh
+++ atak-civ-4.9.0.2/atak-config.sh
@@ -4,6 +4,6 @@
 keytool -genkey -v -keystore my-release-key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias my-alias -keypass android -storepass android -dname "CN=Android Developer, OU=DevOps, O=atak-civ-4.8.1.5, C=US, L=Ft. Belvoir"
 # NOTE : use `android´ for your password, type the cert info (just hit enter), enventually type yes
 
-ln -s ~/atak/atak-civ-4.8.1.5/my-release-key.jks ~/atak/atak-civ-4.8.1.5/plugin-examples/plugintemplate/app/my-release-key.jks
+ln -s ~/atak/atak-civ/my-release-key.jks ~/atak/atak-civ/plugins/plugintemplate/app/my-release-key.jks
 
-ln -s ~/atak/atak-civ-4.8.1.5/local.properties ~/atak/atak-civ-4.8.1.5/plugin-examples/plugintemplate/local.properties
+ln -s ~/atak/atak-civ/local.properties ~/atak/atak-civ/plugins/plugintemplate/local.properties
